532b981937569f28b6b5f4b6bf375922f153c03a
rohit0208-design <bhatturohitkumar@gmail.com>
2026-01-07 17:02:31 +0530

    Add files via upload

---
+++ day2.py
@@ -0,0 +1,22 @@
+Python 3.13.11 (v3.13.11:627894459a8, Dec  5 2025, 11:26:37) [Clang 16.0.0 (clang-1600.0.26.6)] on darwin
+Enter "help" below or click "Help" above for more information.
+>>> name = "rohit kumar "
+>>> age = 40
+>>> list_1=[1,2,3,4,5,6,3,2,"hi",7,2,(1,2,3,4)]
+>>> tuple1=(10,30,40,"welcome","hello")#tuple of items
+>>> cards={"A",2,3,4,5,6,8,9,"j","Q","K",2,3,4,5,6,7,8}
+>>> student_marks={"maths":90,"english":60,"social":70,"telugu":35}#dict datatype
+>>> print("my name is ":,name )
+SyntaxError: invalid syntax
+>>> print("my name is :",name)
+my name is : rohit kumar 
+>>> print("my age is:", age)
+my age is: 40
+>>> print("list is :",list_1)
+list is : [1, 2, 3, 4, 5, 6, 3, 2, 'hi', 7, 2, (1, 2, 3, 4)]
+>>> print("tuple of items are :",tuple1)
+tuple of items are : (10, 30, 40, 'welcome', 'hello')
+>>> print("pack of cards are :",cards)
+pack of cards are : {'j', 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'Q', 'K'}
+>>> print("s1 marks are :",student_marks)
+s1 marks are : {'maths': 90, 'english': 60, 'social': 70, 'telugu': 35}
