18106f69c076a241e9347f93e724f07248416c0b
rohit0208-design <bhatturohitkumar@gmail.com>
2026-01-08 16:52:12 +0530

    Add files via upload
    commit changes

---
+++ day 3 simple calculator.py
@@ -0,0 +1,12 @@
+#simple calculator operations
+# variable and operators
+a=10
+b=20
+print("sum of numbers is:", a+b)
+print("subtraction of two numbers is :", a-b)
+print("multiplication of two numbers is :", a*b)
+print("divison of two numbers is :",b/a)
+print("floor division of two numbers :",b//a)
+print("modulo division of two numbers :",b%a)
+print("exponent of two numbers :",b**a)
+
