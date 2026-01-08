0d46657dff38a3ce353928d7a3b7e4a71f991095
rohit0208-design <bhatturohitkumar@gmail.com>
2026-01-08 20:41:41 +0530

    Add files via upload

--- logical day 3 .py
+++ logical day 3 .py
@@ -3,3 +3,9 @@ y = False
 print("x and y :", x and y )
 print("x or y :", x or y )
 print(" not x :", not x )
+
+#ANDOR opertions using numbers 
+a = 10
+b = 20
+print (a>5 and b>15)
+print(a>30 or b>18)
