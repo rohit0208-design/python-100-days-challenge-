3d7cfc03121125730a206365b941faab45220a24
rohit0208-design <bhatturohitkumar@gmail.com>
2026-01-17 15:41:47 +0530

    Add files via upload

---
+++ day 8 while loop .py
@@ -0,0 +1,22 @@
+# reverse order of numbers using while loop
+i = 10
+while i >=1:
+    print(i)
+    i -=1
+#print odd number from 1 to 20 usoing while loop 
+i = 1
+while i <=20:
+    if i%2 !=0:
+        print(i)
+    i +=1 
+# printing sqaure of numbers using while loop 
+i = 1
+while i<=5:
+    print(i**2)
+    i+=1
+# printing even numbers from 1 to 20 using while loop 
+i = 1
+while i <=20:
+    if i%2==0:
+        print(i)
+    i +=1 
